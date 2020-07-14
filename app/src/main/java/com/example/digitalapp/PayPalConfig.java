233a8c5d54bbbb171204c6b1e0452a6d9d6ea9d9
Anne-15 <wariiyuanne@gmail.com>
2020-07-15 00:04:30 +0300

    Semi final update to the project.

---
+++ app/src/main/java/com/example/digitalapp/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.example.digitalapp;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AZHkI7XNDyszQmJ0szCA6NzHZp1VxbQRJQxXRTsX-2qGX0bN19E1i6lEsyx3hJDDo5bQA42YzAu49b29";
+}
